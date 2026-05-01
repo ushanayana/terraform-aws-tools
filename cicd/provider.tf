e03367df5fa52c51956f0cb5e621b0e3036e5ab4
ushanayana <ushanayana@gmail.com>
2026-05-01 22:43:17 +0530

    jenkins

--- cicd/provider.tf
+++ cicd/provider.tf
@@ -6,7 +6,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket = "daws78s-remote-state"
+    bucket = "78s-remote-state"
     key    = "jenkins"
     region = "us-east-1"
     #dynamodb_table = "daws78s-locking"
