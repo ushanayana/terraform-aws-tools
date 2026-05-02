93d7427539e8732424f1e85db182130f0ec3b9c1
ushanayana <ushanayana@gmail.com>
2026-05-02 10:12:06 +0530

    terraform-jenkins

--- cicd/jenkins.sh
+++ cicd/jenkins.sh
@@ -1,21 +1,21 @@
-#!/bin/bash
-yum update -y
-yum install -y wget
-
-wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-
-yum install -y fontconfig java-17-openjdk jenkins
-
-systemctl daemon-reload
-systemctl enable jenkins
-systemctl start jenkins
-
-
 # #!/bin/bash
-# curl -o /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+# yum update -y
+# yum install -y wget
+
+# wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 # rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-# yum install fontconfig java-17-openjdk jenkins -y
+
+# yum install -y fontconfig java-17-openjdk jenkins
+
 # systemctl daemon-reload
 # systemctl enable jenkins
 # systemctl start jenkins
+
+
+#!/bin/bash
+sudo curl -o /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo yum install fontconfig java-17-openjdk jenkins -y
+sudo systemctl daemon-reload
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
