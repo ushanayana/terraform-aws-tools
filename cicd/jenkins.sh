71757a2b164025215fc72522f18c915eda546a2a
ushanayana <ushanayana@gmail.com>
2026-05-02 09:57:17 +0530

    terraform-jenkins

--- cicd/jenkins.sh
+++ cicd/jenkins.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
-curl -o /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+yum update -y
+yum install -y wget
+
+wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-yum install fontconfig java-17-openjdk jenkins -y
+
+yum install -y fontconfig java-17-openjdk jenkins
+
 systemctl daemon-reload
 systemctl enable jenkins
 systemctl start jenkins
+
+
+# #!/bin/bash
+# curl -o /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+# rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+# yum install fontconfig java-17-openjdk jenkins -y
+# systemctl daemon-reload
+# systemctl enable jenkins
+# systemctl start jenkins
