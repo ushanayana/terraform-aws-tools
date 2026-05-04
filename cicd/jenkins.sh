c8688d0f958fc479c1f6dce200fbf46d3128ea86
ushanayana <ushanayana@gmail.com>
2026-05-04 11:57:23 +0530

    jenkins

--- cicd/jenkins.sh
+++ cicd/jenkins.sh
@@ -2,6 +2,7 @@
 curl -o /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 yum install fontconfig java-17-openjdk jenkins -y
+yum install jenkins
 systemctl daemon-reload
 systemctl enable jenkins
 systemctl start jenkins
