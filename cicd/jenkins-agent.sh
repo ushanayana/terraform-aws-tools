4e31d35a73d07d42259d17bc1a933c8cee8665f5
ushanayana <ushanayana@gmail.com>
2026-05-04 19:50:04 +0530

    jenkins

--- cicd/jenkins-agent.sh
+++ cicd/jenkins-agent.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-yum install fontconfig java-17-openjdk -y
+yum install fontconfig java-21-openjdk -y
 # yum install -y yum-utils
 # yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
 # yum -y install terraform
--- cicd/jenkins.sh
+++ cicd/jenkins.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 curl -o /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-yum install fontconfig java-17-openjdk jenkins -y
+yum install fontconfig java-21-openjdk jenkins -y
 yum install jenkins
 systemctl daemon-reload
 systemctl enable jenkins
