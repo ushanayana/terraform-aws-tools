23db287276941ec539d426d20e58973ee24e935c
ushanayana <ushanayana@gmail.com>
2026-05-06 10:29:22 +0530

    jenkins

--- cicd/data.tf
+++ cicd/data.tf
@@ -18,3 +18,24 @@ data "aws_ami" "ami_info" {
         values = ["hvm"]
     }
 }
+
+data "aws_ami" "nexus_ami_info" {
+
+    most_recent = true
+    owners = ["679593333241"]
+
+    filter {
+        name   = "name"
+        values = ["SolveDevOps-Nexus-Server-Ubuntu24.04-20260420-*"]
+    }
+
+    filter {
+        name   = "root-device-type"
+        values = ["ebs"]
+    }
+
+    filter {
+        name   = "virtualization-type"
+        values = ["hvm"]
+    }
+}
--- cicd/main.tf
+++ cicd/main.tf
@@ -29,6 +29,38 @@ module "jenkins_agent" {
   }
 }
 
+resource "aws_key_pair" "tools" {
+  key_name   = "tools"
+  # you can paste the public key directly like this
+  #public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL/EYXhkubgb3WuAjDsaTWNcuF76fP24dApVykLRJvql Usha@DESKTOP-09GJKR9"
+  public_key = file("~/.ssh/nexus.pub")
+  # ~ means windows home directory
+}
+
+
+module "nexus" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+
+  name = "nexus"
+
+  instance_type          = "t3.small"
+  vpc_security_group_ids = ["sg-0cab6b9e7e0937b11"] #replace your SG
+  subnet_id = "subnet-003f9e3a9ec29d47a" #replace your Subnet
+  ami = data.aws_ami.nexus_ami_info.id
+  key_name = aws_key_pair.tools.key_name
+  root_block_device = [
+    {
+      volume_type = "gp3"
+      volume_size = 30
+    }
+  ]
+  tags = {
+    Name = "nexus"
+  }
+  
+  
+}
+
 module "records" {
   source  = "terraform-aws-modules/route53/aws//modules/records"
   version = "~> 2.0"
@@ -54,16 +86,16 @@ module "records" {
       ]
       allow_overwrite = true
     },
-    # {
-    #   name    = "nexus"
-    #   type    = "A"
-    #   ttl     = 1
-    #   allow_overwrite = true
-    #   records = [
-    #     module.nexus.private_ip
-    #   ]
-    #   allow_overwrite = true
-    # }
+    {
+      name    = "nexus"
+      type    = "A"
+      ttl     = 1
+      allow_overwrite = true
+      records = [
+        module.nexus.private_ip
+      ]
+      allow_overwrite = true
+    }
   ]
 
 }
